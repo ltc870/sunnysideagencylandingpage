0a0d9d3dd7237a20c02c891df76677cc767875cc
Lawrence C <lawrencecaudle870@gmail.com>
2022-12-12 16:34:23 -0500

    added Navbar component and Navbar component css

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <>
+            <li>
+                <a href="#">About</a>
+            </li>
+            <li>
+                <a href="#">Services</a>
+            </li>
+            <li>
+                <a href="#">Projects</a>
+            </li>
+            <li>
+                <a href="#">Contacts</a>
+            </li>
+        </>
+    );
+};
+
+export default Navbar;
