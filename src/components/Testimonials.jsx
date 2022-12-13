eeac8d81e95a7997c53b87c7d5d60f6840328b9a
Lawrence C <lawrencecaudle870@gmail.com>
2022-12-13 09:14:04 -0500

    added srcSet to images to make them responsive

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -3,9 +3,13 @@ import emily from "../images/image-emily.jpg";
 import thomas from "../images/image-thomas.jpg";
 import jennie from "../images/image-jennie.jpg";
 import milk from "../images/mobile/image-gallery-milkbottles.jpg";
+import milk2 from "../images/desktop/image-gallery-milkbottles.jpg";
 import orange from "../images/mobile/image-gallery-orange.jpg";
+import orange2 from "../images/desktop/image-gallery-orange.jpg";
 import cone from "../images/mobile/image-gallery-cone.jpg";
+import cone2 from "../images/desktop/image-gallery-cone.jpg";
 import sugar from "../images/mobile/image-gallery-sugar-cubes.jpg";
+import sugar2 from "../images/desktop/image-gallery-sugarcubes.jpg";
 import classes from "./Testimonials.module.css";
 
 const Testimonials = () => {
@@ -64,24 +68,28 @@ const Testimonials = () => {
                 <img
                     className={classes.galleryPicture}
                     src={milk}
+                    srcSet={`${milk} 376w, ${milk2} 720w`}
                     alt="milk bottles"
                 />
 
                 <img
                     className={classes.galleryPicture}
                     src={orange}
+                    srcSet={`${orange} 376w, ${orange2} 720w`}
                     alt="an orange"
                 />
 
                 <img
                     className={classes.galleryPicture}
                     src={cone}
+                    srcSet={`${cone} 376w, ${cone2} 720w`}
                     alt="an ice cream cone"
                 />
 
                 <img
                     className={classes.galleryPicture}
                     src={sugar}
+                    srcSet={`${sugar} 376w, ${sugar2} 720w`}
                     alt="sugar cubes"
                 />
             </div>
