744d40adb113180484a0c88180f4de68f530d0a1
Lawrence C <lawrencecaudle870@gmail.com>
2022-12-16 16:29:10 -0500

    corrected vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/sunnysideagencylandingpage",
+    base: "/sunnysideagencylandingpage/",
     css: {
         postcss: {
             plugins: [autoprefixer],
