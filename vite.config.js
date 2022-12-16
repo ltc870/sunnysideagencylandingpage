1cb02b14fb62a4faa9c370f2739f9d4e010bd867
Lawrence C <lawrencecaudle870@gmail.com>
2022-12-16 16:22:47 -0500

    added base and css to vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+    base: "/sunnysideagencylandingpage",
+    css: {
+        postcss: {
+            plugins: [autoprefixer],
+        },
+    },
+    plugins: [react()],
+});
