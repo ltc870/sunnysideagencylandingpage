9d6891564e30016957d27834e6f459817ab9f55d
Lawrence C <lawrencecaudle870@gmail.com>
2022-12-18 15:53:39 -0500

    removed Gallery code out of Testimonials files

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -2,14 +2,6 @@ import React from "react";
 import emily from "../images/image-emily.jpg";
 import thomas from "../images/image-thomas.jpg";
 import jennie from "../images/image-jennie.jpg";
-import milk from "../images/mobile/image-gallery-milkbottles.jpg";
-import milk2 from "../images/desktop/image-gallery-milkbottles.jpg";
-import orange from "../images/mobile/image-gallery-orange.jpg";
-import orange2 from "../images/desktop/image-gallery-orange.jpg";
-import cone from "../images/mobile/image-gallery-cone.jpg";
-import cone2 from "../images/desktop/image-gallery-cone.jpg";
-import sugar from "../images/mobile/image-gallery-sugar-cubes.jpg";
-import sugar2 from "../images/desktop/image-gallery-sugarcubes.jpg";
 import classes from "./Testimonials.module.css";
 
 const Testimonials = () => {
@@ -63,39 +55,6 @@ const Testimonials = () => {
                         Business Owner
                     </p>
                 </div>
-            </div>
-            <div className={classes.gallery}>
-                <img
-                    className={classes.galleryPicture}
-                    src={milk}
-                    srcSet={`${milk} 376w, ${milk2} 720w`}
-                    sizes="(min-width: 800px) 720px, 376px"
-                    alt="milk bottles"
-                />
-
-                <img
-                    className={classes.galleryPicture}
-                    src={orange}
-                    srcSet={`${orange} 376w, ${orange2} 720w`}
-                    sizes="(min-width: 800px) 720px, 376px"
-                    alt="an orange on a white plate with green trim"
-                />
-
-                <img
-                    className={classes.galleryPicture}
-                    src={cone}
-                    srcSet={`${cone} 376w, ${cone2} 720w`}
-                    sizes="(min-width: 800px) 720px, 376px"
-                    alt="an ice cream cone"
-                />
-
-                <img
-                    className={classes.galleryPicture}
-                    src={sugar}
-                    srcSet={`${sugar} 376w, ${sugar2} 720w`}
-                    sizes="(min-width: 800px) 720px, 376px"
-                    alt="sugar cubes stacked atop each other"
-                />
             </div>
         </section>
     );
