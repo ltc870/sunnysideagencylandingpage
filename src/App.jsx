963de54d95ccacfed1ac8900204f24d94691368d
Lawrence C <lawrencecaudle870@gmail.com>
2022-12-18 15:52:40 -0500

    imported Gallery component into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Testimonials from "./components/Testimonials";
+import Gallery from "./components/Gallery";
 import Footer from "./components/Footer";
 import "./App.css";
 
@@ -11,6 +12,7 @@ const App = () => {
             <Header />
             <Main />
             <Testimonials />
+            <Gallery />
             <Footer />
         </div>
     );
